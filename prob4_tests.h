a23720a84d7082572efdbe3446bcaa87a4efb5d5
Kalin Georgiev <kalingeorgiev@Alfred.local>
2019-11-11 22:39:13 +0200

    Formatting sanitation

--- prob4_tests.h
+++ prob4_tests.h
@@ -10,21 +10,17 @@ TEST_CASE("Empty list")
 }
 
 TEST_CASE("One element")
-{
-    
+{  
     node<int> *l = tolist<int>({1});
     mirror (l);
     CHECK(tovector(l) == std::vector<int>({1, 1}));
-
 }
 
 TEST_CASE("Three elements")
 {
-    
     node<int> *l = tolist<int>({1,2,3});
     mirror (l);
     CHECK(tovector(l) == std::vector<int>({1,2,3, 3,2,1}));
-
 }
 
 #endif
